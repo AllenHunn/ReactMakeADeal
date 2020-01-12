220d2e6f41e4e88588d68d346e3f58ebb7b4c933
Allen Hunn <AllenHunn@gmail.com>
2020-01-12 09:37:45 -0500

    initial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Random } from 'random-js';
+import { GridListTile, GridList, makeStyles } from '@material-ui/core';
+import * as R from 'ramda';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+    overflow: 'hidden',
+    backgroundColor: theme.palette.background.paper,
+  },
+  gridList: {
+    width: 500
+  },
+  background: {
+    backgroundImage: 'url(briefcase.png)'
+  }
+}));
+
+function generateCases(numOfCases, startingDollarAmount) {
+  const availableCases = R.range(1, numOfCases+1);
+  new Random().shuffle(availableCases);
+  const cases = [];
+  let dollarAmmount = startingDollarAmount;
+  for(let i = 1; i <= numOfCases; i++) {
+    cases[i-1] = {
+      dollarAmmount,
+      caseNumber: availableCases[i-1]
+    }
+    dollarAmmount *= 2;
+  }
+
+  return cases;
+}
 
 function App() {
+  const cases = R.sortBy(R.prop('caseNumber'), generateCases(12, 250));
+  const classes = useStyles();
+  console.log(cases);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.root}>
+      <GridList cellHeight={268} className={classes.gridList} cols={3}>
+        {cases.map((tile) => (
+          <GridListTile key={tile.containingCase} cols={1}>
+            <span className={classes.background}>
+              {tile.caseNumber} {tile.dollarAmmount}
+            </span>
+          </GridListTile>
+        ))}
+      </GridList>
     </div>
   );
 }
